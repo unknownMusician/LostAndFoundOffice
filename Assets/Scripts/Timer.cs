d6d029ed3126f1cb4a40f3d7c4a0cafd9e3c2a27
unknownMusician <serega3assassin2002@gmail.com>
2021-01-28 17:39:16 +0200

    timer fixes

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,48 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace Assets.Scripts
-{
-    public class Timer : MonoBehaviour
-    {
-        public GameObject Time;
-        public GameObject Sprite;
-        public delegate void TimerDelegate();
-        public TimerDelegate TimeOver;
-        private static Timer _timer = null;
-        private float StartTime { get; set; } = 120;
-        private float CurrentTime { get; set; } = 120;
+public sealed class Timer : MonoBehaviour {
 
-        private Timer()
-        {
+    [SerializeField] private TextMesh Time = default;
+    [SerializeField] private float seconds = default;
+    [SerializeField] private Material timerMaterial = default;
 
+    public UnityAction TimeOver;
+
+    private static Timer _timer = null;
+
+    private Timer() { }
+
+    public static Timer GetSingleton() {
+        if (_timer == null) { _timer = new Timer(); }
+        return _timer;
+    }
+
+    private void Start() => StartCoroutine(TimerTick(seconds));
+
+    private void Tick(float currentTime, float startTime) {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        Time.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerMaterial.SetFloat("Lerp", currentTime / startTime);
+    }
+
+    private IEnumerator TimerTick(float startTime) {
+        for (float currentTime = startTime; currentTime > 0; currentTime -= UnityEngine.Time.deltaTime) {
+            Tick(currentTime, startTime);
+            yield return null;
         }
-
-        public static Timer GetSingleton()
-        {
-            if (_timer == null)
-                _timer = new Timer();
-            return _timer;
-        }
-
-        protected void Update()
-        {
-            CurrentTime -= UnityEngine.Time.deltaTime;
-            Tick(CurrentTime);
-        }
-
-
-        public void Tick(float currentTime)
-        {
-            float minutes = Mathf.FloorToInt(currentTime / 60);
-            float seconds = Mathf.FloorToInt(currentTime % 60);
-            Time.GetComponent<TextMesh>().text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        }
-
+        Tick(0, startTime);
+        TimeOver?.Invoke();
     }
 }

37e877a3572cb422d7790ff10b685d1ed4457696
unknownMusician <serega3assassin2002@gmail.com>
2021-01-28 21:46:24 +0200

    s

--- Assets/Scripts/Interaction/CustomerWindow.cs
+++ Assets/Scripts/Interaction/CustomerWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Interaction.Service;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -41,13 +42,35 @@ namespace Interaction {
             print($"Received order from customer: \"{customer}\"");
             this.customer = customer;
             customerGavePainting = true; // TODO
+            waitCoroutine = StartCoroutine(WaitCoroutine(customer));
             // TODO
         }
 
         private void SendItemToManager(Item item) {
-            item.Grab(customer.transform, Vector3.forward);
-            customer.ReceiveAnswer(true);
+            if (item != null) {
+                item.Grab(customer.transform, Vector3.forward);
+                customer.ReceiveAnswer(true);
+            } else {
+                customer.ReceiveAnswer(null);
+            }
+            if (waitCoroutine != null) {
+                StopCoroutine(waitCoroutine);
+                waitCoroutine = null;
+            }
+            customer = null;
             // TODO
+
+        }
+
+        private Coroutine waitCoroutine = null; // todo
+        private IEnumerator WaitCoroutine(CustomerSpawning.Customer customer) { // todo
+            for (int i = 5; i > 0; i++) {
+                yield return new WaitForSeconds(1);
+                print(i);
+            }
+            customer.ReceiveAnswer(null);
+            SendItemToManager(null);
+            waitCoroutine = null;
         }
     }
 }

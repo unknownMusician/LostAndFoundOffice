e3d31b89eb0c1353cf124410038f58cab2d38c86
Yaroslav Hryhorenko <yarik13371337@gmail.com>
2021-01-28 22:34:50 +0200

    f

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -9,28 +9,39 @@ namespace Assets.Scripts
 {
     public class Manager : MonoBehaviour
     {
-        public List<Texture> Images = new List<Texture>();
+        public List<Texture2D> Images = new List<Texture2D>();
         public List<GameObject> Items = new List<GameObject>();
         public List<GameObject> AnswerItems = new List<GameObject>();
         private int MismatchItems { get; set; } = 0;
 
         protected void Start()
         {
-            GetItems();
+            GetItemsAndImages();
             DeliverATruckOfItems();
             Timer.GetSingleton().StartTimer(Items.Count * 15);
             Timer.GetSingleton().TimeOver += Finish;
         }
 
-        private void GetItems()
+        private void GetItemsAndImages()
         {
-            //Resources.LoadAll()
-            //Generator.GetItemObject()
+            //var itemPrefabs = Resources.LoadAll("Assets/Resources/Prefabs/Items");
+            //foreach (var o in itemPrefabs)
+            //{
+            //    Items.Add(Generator.GetItemObject((GameObject)o,)); 
+            //}
+            //var imagePrefabs = Resources.LoadAll("Assets/Resources/Prefabs/Items");
+            //foreach (var o in imagePrefabs)
+            //{
+            //    Images.Add(Generator.GetPainting((Texture2D)o,));
+            //}
         }
 
         private void DeliverATruckOfItems()
         {
-
+            foreach (GameObject item in Items)
+            {
+                Instantiate(item, new Vector3(0, 5, 0), Quaternion.identity);
+            }
         }
 
         private void Finish()

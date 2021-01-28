508082c81fc0cd9790240d4dc4c979ce20fe1bd1
unknownMusician <serega3assassin2002@gmail.com>
2021-01-28 21:27:22 +0200

    Update Generator.cs

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,9 +3,11 @@
 public static class Generator {
     public static Texture2D GetPainting(Texture2D rgbPainting, params Color[] colors) {
         // todo
+        return rgbPainting;
     }
 
     public static GameObject GetItemObject(GameObject model, params Material[] materials) {
         // todo
+        return model;
     }
 }

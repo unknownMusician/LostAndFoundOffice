4fdfe6a821da61a0e043e997bc2c4fc74ab38fcf
Yaroslav Hryhorenko <yarik13371337@gmail.com>
2021-01-31 00:39:02 +0200

    suka

--- Assets/Scripts/ShowBook.cs
+++ Assets/Scripts/ShowBook.cs
@@ -12,8 +12,12 @@ namespace Assets.Scripts
 
         private void Awake() => Manager.Fin += ShowAll;
 
-        public void ShowAll() {
-            for (int i = 0; i < ComplaintBook.Answers.Length; i++) { ShowNext(i); }
+        public void ShowAll()
+        {
+            for (int i = 0; i < ComplaintBook.Answers.Length; i++)
+            {
+                ShowNext(i);
+            }
         }
 
         public void ShowNext(int index)
@@ -26,12 +30,14 @@ namespace Assets.Scripts
                     GameObject correct = Manager.ItemInfos[ind].model;
                     if (correct != null)
                     {
+                        correct = Instantiate(correct);
                         Destroy(correct.gameObject.GetComponent<Item>());
-                        Destroy(correct.gameObject.GetComponent<Rigidbody>());
+                        //Destroy(correct.gameObject.GetComponent<Rigidbody>());
                         correct.transform.position =
-                            new Vector3(AnswerObject.transform.position.x - 5,
-                                AnswerObject.transform.position.y,
+                            new Vector3(AnswerObject.transform.position.x - 1.2f,
+                                AnswerObject.transform.position.y - 0.3f * index,
                                 AnswerObject.transform.position.z);
+                        correct.transform.rotation = Quaternion.Euler(-45,45,0);
                     }
                 }
 
@@ -40,12 +46,14 @@ namespace Assets.Scripts
                 if (ComplaintBook.Answers[index].givenId != -1)
                 {
                     answer = Manager.ItemInfos[ComplaintBook.Answers[index].givenId].model;
+                    answer = Instantiate(answer);
                     Destroy(answer.gameObject.GetComponent<Item>());
-                    Destroy(answer.gameObject.GetComponent<Rigidbody>());
+                    //Destroy(answer.gameObject.GetComponent<Rigidbody>());
                     answer.transform.position =
-                        new Vector3(AnswerObject.transform.position.x + 5,
-                            AnswerObject.transform.position.y,
+                        new Vector3(AnswerObject.transform.position.x + 1.2f,
+                            AnswerObject.transform.position.y - 0.3f * index,
                             AnswerObject.transform.position.z);
+                    answer.transform.rotation = Quaternion.Euler(-45, 45, 0);
                 }
             }
         }

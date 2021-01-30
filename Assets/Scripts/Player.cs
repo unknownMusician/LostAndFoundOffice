aa9f94482dec89767c144ad0a57c1986dd89d120
Yaroslav Hryhorenko <yarik13371337@gmail.com>
2021-01-30 14:48:52 +0200

    f

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Interacter))]
+[RequireComponent(typeof(Animator))]
 public sealed class Player : MonoBehaviour {
     private Rigidbody rigidBody = null;
     private Interacter interacter = null;
+    private Animator animator = null;
 
     [SerializeField] private float speed;
 
@@ -18,6 +20,7 @@ public sealed class Player : MonoBehaviour {
     private void Awake() {
         rigidBody = GetComponent<Rigidbody>();
         interacter = GetComponent<Interacter>();
+        animator = transform.Find("Model").GetComponent<Animator>();
     }
 
     private void Update() {
@@ -27,7 +30,7 @@ public sealed class Player : MonoBehaviour {
 
     private void MoveUpdate() {
         Vector2 move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
-
+        animator.SetFloat("currentSpeed",move.magnitude);
         rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, new Vector3(move.x, rigidBody.velocity.y, move.y), ref moveDampVelocity, 0.1f);
         rigidBody.angularVelocity = Vector3.zero;
         if (move.magnitude == 0) { return; }
